3da643122c32adbcf5f588ddf4f65f088fb286e2
Omega X Rae <omega.x.rae@gmail.com>
2022-03-28 23:19:54 -0700

    Create april_1st.sh

---
+++ april_1st.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt-get update && apt-get upgrade 
+apt-get install nginx 
+cd /var/www 
+git clone https://github.com/virtualpilotlight/april_1st.git
+chmod 755 -R /var/www 
+cd aprils_1st 
+mv /var/www/april_1st/index.nginx-debian.html /var/www/html/index.nginx-debian.html
