39ac08df5b8c6258928020a27ef19bdb56ffe164
Omega X Rae <omega.x.rae@gmail.com>
2022-03-29 17:44:34 -0700

    fixing script

--- april_1st.sh
+++ april_1st.sh
@@ -5,5 +5,5 @@ apt-get install nginx
 cd /var/www 
 git clone https://github.com/virtualpilotlight/april_1st.git
 chmod 755 -R /var/www 
-cd aprils_1st 
+cd april_1st 
 mv /var/www/april_1st/index.nginx-debian.html /var/www/html/index.nginx-debian.html
